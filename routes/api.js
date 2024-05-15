981f8aec1f6c68f3150374c4fb5acff01554d708
MacKenzie Clark <clark.mac99@gmail.com>
2024-05-15 22:23:55 +0000

    feat: get route added

--- routes/api.js
+++ routes/api.js
@@ -7,9 +7,31 @@ module.exports = function (app) {
 
   app.route('/api/issues/:project')
   
-    .get((req, res) => {
-      let project = req.params.project;
+    .get(async (req, res) => {
+      let projectName = req.params.project;
       
+      try {
+        const project = await ProjectModel.findOne({ name: projectName });
+        if (!project) {
+          res.json({ error: "project not found" });
+          return;
+        } else {
+          const issues = await IssueModel.find({
+            projectId: project._id,
+            ...req.query,
+          });
+          if (!issues) {
+            res.json({ error: "no issues found" });
+            return;
+          } else {
+            res.json(issues);
+            return;
+          }
+        }
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err });
+      }
     })
     
     .post(async (req, res) => {
@@ -36,8 +58,11 @@ module.exports = function (app) {
           updated_on: new Date(),
           created_by: created_by || "",
           assigned_to: assigned_to || "",
+          open: true,
           status_text: status_text || "",
-        })
+        });
+        const issue = await issueModel.save();
+        res.json(issue);
       } catch (err) {
         console.error(err);
         res.status(500).json({ error: err });
@@ -45,9 +70,18 @@ module.exports = function (app) {
       
     })
     
-    .put((req, res) => {
-      let project = req.params.project;
+    .put(async (req, res) => {
+      let projectName = req.params.project;
       
+      const {
+        _id,
+        issue_title,
+        issue_text,
+        created_by,
+        assigned_to,
+        status_text,
+        open,
+      } = req.body;
     })
     
     .delete((req, res) => {
