4f74d98ef89b377ddd427762cb14a2d421bf5eeb
MacKenzie Clark <clark.mac99@gmail.com>
2024-05-15 22:08:03 +0000

    feat: Models and api post method

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const IssueModel = require('../models').Issue;
+const ProjectModel = require('../models').Project;
+
 module.exports = function (app) {
 
   app.route('/api/issues/:project')
@@ -9,10 +12,36 @@ module.exports = function (app) {
       
     })
     
-    .post((req, res) => {
-      let project = req.params.project;
+    .post(async (req, res) => {
+      let projectName = req.params.project;
 
-      res.json(project);
+      const { issue_title, issue_text, created_by, assigned_to, status_text } = req.body;
+
+      if (!issue_title || !issue_text || !created_by) {
+        res.json({ error: "required field(s) missing" });
+        return;
+      }
+
+      try {
+        let projectModel = await ProjectModel.findOne({ name: projectName });
+        if (!projectModel) {
+          projectModel = new ProjectModel({ name: projectName });
+          projectModel = await projectModel.save();
+        }
+        const issueModel = new IssueModel({
+          projectId: projectModel._id,
+          issue_title: issue_title || "",
+          issue_text: issue_text || "",
+          created_on: new Date(),
+          updated_on: new Date(),
+          created_by: created_by || "",
+          assigned_to: assigned_to || "",
+          status_text: status_text || "",
+        })
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err });
+      }
       
     })
     
